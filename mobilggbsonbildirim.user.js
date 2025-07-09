d8ce0ed4465d9e2a4086b375d807e49c8e604257
Ercan Erden <ercerd@gmail.com>
2025-07-09 15:17:19 +0300

    mobilde ID görünmesi için script

---
+++ mobilggbsonbildirim.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Show Hidden Columns on GGB Son Bildirim
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Show hidden Gümrük Başvuru No and Gümrük Başvuru Tarihi columns on ggbsonbildirim.tarimorman.gov.tr
+// @author       Grok
+// @match        https://ggbsonbildirim.tarimorman.gov.tr/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // CSS enjeksiyonu ile medya sorgularını geçersiz kıl
+    const style = document.createElement('style');
+    style.innerHTML = `
+        @media only screen and (max-width: 1100px) {
+            th:nth-child(7), th:nth-child(8) {
+                display: table-cell !important;
+                width: auto !important;
+                min-width: 100px !important;
+            }
+            td:nth-child(7), td:nth-child(8) {
+                display: table-cell !important;
+                width: auto !important;
+                min-width: 100px !important;
+            }
+        }
+        th[style*="display: none"], td[style*="display: none"] {
+            display: table-cell !important;
+            width: auto !important;
+            min-width: 100px !important;
+        }
+    `;
+    document.head.appendChild(style);
+
+    // Doğrudan stil özniteliklerini kaldır
+    document.querySelectorAll('th[style*="display: none"], td[style*="display: none"]').forEach(element => {
+        element.style.display = 'table-cell';
+        element.style.width = '100px';
+        element.style.minWidth = '100px';
+    });
+})();
