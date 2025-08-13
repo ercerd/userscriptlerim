bb59bae66a6abf7ec314361918490143ccc8d8ce
Ercan Erden <ercerd@gmail.com>
2025-08-13 16:23:58 +0300

    feat(combinedonbildirim): Butonların konumunu içeriğe göre ayarla ve versiyonu güncelle

--- combinedonbildirim.user.js
+++ combinedonbildirim.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Önbildirim GGBS için kullanıcı betiğim
-// @version      2.5
+// @version      2.6
 // @description  All-in-one functionality: captcha autofill, form field updates, buttons for different operations, and sertifika handling
 // @author       Ercan Erden (Modified)
 // @grant        none
